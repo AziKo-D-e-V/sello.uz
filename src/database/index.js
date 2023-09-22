ceeb6b1b69442ee718700c368fdbb1da8c6ecc2c
Azizjon <131476746+AziKo-D-e-V@users.noreply.github.com>
2023-09-22 19:51:30 +0500

    Update password index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
-  "postgres://postgres:Azizjon.0107@localhost:5432/sello"
+  "postgres://postgres:postgres@localhost:5432/sello"
 );
 
 module.exports = sequelize;
