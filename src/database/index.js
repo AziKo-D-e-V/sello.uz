ad530f68a318a0e7e4bc95619c52506cee311d7d
Azizjon <131476746+AziKo-D-e-V@users.noreply.github.com>
2023-09-22 19:53:10 +0500

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
-  "postgres://postgres:postgres@localhost:5432/sello"
+  "postgres://postgres:admin@localhost:5432/sello"
 );
 
 module.exports = sequelize;
